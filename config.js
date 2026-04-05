d9425a3fc3403627f4d8201d0bac07e3403f6446
murtada9780-droid <murtada9780@gmail.com>
2026-04-05 16:08:52 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,1 @@
-const CONFIG = {
-  whatsapp: "9647770998885",
-  instagram: "luvra.iq",
-  tiktok: "luvra.iq",
 
-  maxOrdersPerDay: 10,
-
-  packages: [
-    {name: "Simple Gift", price: "$3"},
-    {name: "Emotional Experience", price: "$5"},
-    {name: "Cinematic Story", price: "$10"}
-  ]
-};
